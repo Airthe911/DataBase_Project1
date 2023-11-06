0342dabc14ad2ddc4ab56f01a69ae11cece82c6a
XiYa Yang <112851246+Lily127Yang@users.noreply.github.com>
2023-11-06 22:24:41 +0800

    图书搜索功能测试最终版
    图书搜索功能测试最终版

--- test_searchbook.py
+++ test_searchbook.py
@@ -19,16 +19,14 @@ class TestSearchBook:
         self.seller_id = "test_operations_seller_{}".format(str(uuid.uuid1()))
         self.buyer_id = "test_operations_buyer_{}".format(str(uuid.uuid1()))
         self.store_id = "test_operations_store_{}".format(str(uuid.uuid1()))
-        self.exist_store_id = "111"
         self.seller_password = self.seller_id
         self.buyer_password = self.buyer_id
         self.user_password = self.user_id
         self.buyer = register_new_buyer(self.buyer_id, self.buyer_password)
         self.d_op = direct_operations.Operations()
-
         self.seller = register_new_seller(self.user_id, self.user_password)
+        self.exist_store_id = "111"
         self.seller.create_store(self.exist_store_id)
-
         book_db = book.BookDB()
         self.books = book_db.get_book_info(0, 2)
         for b in self.books:
@@ -43,14 +41,21 @@ class TestSearchBook:
 
     def test_global_search(self):
         """Test for global search functionality."""
+        # 全局搜索关键词“三毛”
         keyword = "三毛"
         code = self.search_books(keyword)
         assert code == 200, f"Expected 200, but got {code}"
         code, book_list = self.d_op.global_search(keyword)
         assert code == 200, f"Expected 200, but got {code}"
+
+        # 全局搜索不存在的关键词“**”
         keyword = "**"
+        code = self.search_books(keyword)
+        assert code == 915, f"Expected 200, but got {code}"
         code, book_list = self.d_op.global_search(keyword)
         assert code == 915, f"Expected 200, but got {code}"
+
+        # 全局搜索但是不输入关键词
         keyword = ""
         code, book_list = self.d_op.global_search(keyword)
         assert code == 914, f"Expected not 200 for empty keyword, but got {code}"
@@ -62,20 +67,24 @@ class TestSearchBook:
         keyword = "三毛"
         code = self.search_books(keyword, how="local_search", store_id=self.store_id)
         assert code == 200, f"Expected 200, but got {code}"
+        code, book_list = self.d_op.local_search(keyword, store_id=self.exist_store_id)
+        assert code == 200, f"Expected 200, but got {code}"
+
         code, book_list = self.d_op.local_search(keyword, store_id=None)
         assert code == 916, f"Expected 200, but got {code}"
-        keyword = ""
-        code, book_list = self.d_op.local_search(keyword, store_id=self.store_id)
-        assert code == 914, f"Expected 200, but got {code}"
-
-    def test_local_search_without_store_id(self):
-        """Test for local search functionality without providing a store_id."""
-        keyword = "三毛"
         code = self.search_books(keyword, how="local_search", store_id=None)
         assert code == 916, f"Expected 200, but got {code}"
+
         keyword = "不存在的书籍"
         code, book_list = self.d_op.local_search(keyword, store_id=self.exist_store_id)
         assert code == 915, f"Expected 200, but got {code}"
-        keyword = "三毛"
+        code = self.search_books(keyword, how="local_search", store_id=self.exist_store_id)
+        assert code == 915, f"Expected 200, but got {code}"
+
+        keyword = ""
         code, book_list = self.d_op.local_search(keyword, store_id=self.exist_store_id)
-        assert code == 200, f"Expected 200, but got {code}"
+        assert code == 914, f"Expected 200, but got {code}"
+        code = self.search_books(keyword, how="local_search", store_id=self.store_id)
+        assert code == 914, f"Expected 200, but got {code}"
+
+
